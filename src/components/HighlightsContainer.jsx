d6d147fdd13e5a69208ac22c0c6ae538afd61b66
JorgeRochaA <jor2019.cr@gmail.com>
2022-06-03 17:46:22 -0600

    feat: add humidity card

--- src/components/HighlightsContainer.jsx
+++ src/components/HighlightsContainer.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { Fragment } from "react/cjs/react.production.min";
 import "../styles/HighlightsContainer/HighlightsContainer.scss";
+import { Fragment } from "react/cjs/react.production.min";
+import PercentageBar from "./PercentageBar";
+import React from "react";
 function HighlightsContainer({ weather }) {
   return (
     <Fragment>
       {weather && (
         <div className="highlight_container">
           <div className="wind_card">
-            <p className="wind_card_title">Wind status</p>
-            <p className="wind_value">{weather.wind.speed} mph</p>
+            <p className="card_title">Wind status</p>
+            <p className="card_value">{weather.wind.speed} mph</p>
             <div className="img_container">
               <div className="image">
                 <img src={require("../assets/cursor.png")} alt="arrow" />
@@ -16,8 +17,12 @@ function HighlightsContainer({ weather }) {
               WSW
             </div>
           </div>
-          {/* <div className="humidity_card"></div>
-          <div className="visibility_card"></div>
+          <div className="humidity_card">
+            <div className="card_title">Humidity</div>
+            <div className="card_value">{weather.main.humidity}%</div>
+            <PercentageBar humidityPercentage={weather.main.humidity} />
+          </div>
+          {/* <div className="visibility_card"></div>
           <div className="air_card"></div> */}
         </div>
       )}
---
+++ src/components/PercentageBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Fragment } from "react";
+import "../styles/PercentageBar/PercentageBar.scss";
+function PercentageBar({ humidityPercentage }) {
+  return (
+    <Fragment>
+      {humidityPercentage && (
+        <div className="percentageBar_container">
+          <div className="percentage_numbers">
+            <p>0</p>
+            <p>50</p>
+            <p>100</p>
+          </div>
+          <div className="bar">
+            <div
+              className="bar_fill"
+              style={{ width: 229 - (229 - (humidityPercentage / 100) * 229) }}
+            ></div>
+          </div>
+          <div className="percentage_icon">%</div>
+        </div>
+      )}
+    </Fragment>
+  );
+}
+
+export default PercentageBar;
